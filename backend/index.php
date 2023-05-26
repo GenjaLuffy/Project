b57ff175979d1d7bd99e73a4f3f8a1bbb244922d
GenjaLuffy <spawon551@gmail.com>
2023-05-26 21:40:20 +0545

    backend start

---
+++ backend/index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Gaming Top-Up Website</title>
+</head>
+
+<body>
+    <header class="site-header">
+        <div class="container">
+            <div class="header-inner">
+                <img src="./assets/images/LOGO.png" class="sitebrand" alt="" />
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Search</a></li>
+                        <li><a href="#">Cart</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="./loginin.php">LogIn</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <!-- Best seling image and content -->
+    <div class="bestselling">
+        <div class="bestselling-container">
+            <div class="best-selling">
+                <h1 class="bestselling-heading">Best-Selling</h1>
+                <ion-icon name="bag-handle-outline"></ion-icon>
+            </div>
+
+            <div class="img-wrap">
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="PUBG Mobile" />
+                    <h5>PUBG Mobile UC</h5>
+                </div>
+
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="Mobile Legend" />
+                    <h5>Mobile Legend Diamond</h5>
+                </div>
+
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="Free fire" />
+                    <h5>Free Fire Daimond</h5>
+                </div>
+
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="Apex Legend" />
+                    <h5>Apex Legend Coins</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bestselling">
+        <div class="bestselling-container">
+            <div class="best-selling">
+                <h1 class="bestselling-heading">Products</h1>
+                <ion-icon name="bag-handle-outline"></ion-icon>
+            </div>
+
+            <div class="img-wrap">
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="PUBG Mobile" />
+                    <h5>PUBG Mobile UC</h5>
+                </div>
+
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="Mobile Legend" />
+                    <h5>Mobile Legend Diamond</h5>
+                </div>
+
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="Free fire" />
+                    <h5>Free Fire Daimond</h5>
+                </div>
+
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="Apex Legend" />
+                    <h5>Apex Legend Coins</h5>
+                </div>
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="PUBG Mobile" />
+                    <h5>PUBG Mobile UC</h5>
+                </div>
+
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="Mobile Legend" />
+                    <h5>Mobile Legend Diamond</h5>
+                </div>
+
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="Free fire" />
+                    <h5>Free Fire Daimond</h5>
+                </div>
+
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="Apex Legend" />
+                    <h5>Apex Legend Coins</h5>
+                </div>
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="PUBG Mobile" />
+                    <h5>PUBG Mobile UC</h5>
+                </div>
+
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="Mobile Legend" />
+                    <h5>Mobile Legend Diamond</h5>
+                </div>
+
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="Free fire" />
+                    <h5>Free Fire Daimond</h5>
+                </div>
+
+                <div class="best-content">
+                    <img src="./assets/images/img.jpg" alt="Apex Legend" />
+                    <h5>Apex Legend Coins</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="moreproduct">
+        <div class="btn-container">
+            <a href="" class="moreproduct-btn">More Product <ion-icon name="arrow-forward-outline"></ion-icon>
+            </a>
+        </div>
+    </div>
+
+    <footer class="site-footer">
+        <div class="container">
+            <div class="footer-inner">
+                <div class="footer-item">
+                    <img src="../frontend/assets/images/LOGO.png" alt="" />
+                </div>
+                <div class="footer-item">
+                    <h2>PRODUCTS</h2>
+                    <br />
+                    <ul>
+                        <li class="footer-list"><a href="#">Top Up</a></li>
+                        <br />
+                        <li class="footer-list"><a href="#">Gift Cards</a></li>
+                    </ul>
+                </div>
+                <div class="footer-item">
+                    <h2>PAYMENTS</h2>
+                    <a href="#">
+                        <img src="./assets/images/khalti.png" alt="" class="footer-img" />
+                    </a>
+                    <a href="#">
+                        <img src="./assets/images/esewa.png" alt="" class="footer-img" />
+                    </a>
+                </div>
+                <div class="footer-item">
+                    <h2>CONNECT WITH US</h2>
+                    <div class="connect-container">
+                        <a href="#">
+                            <img src="./assets/images/facebook.png" alt="" class="footerconnect-img" />
+                        </a>
+                        <a href="#">
+                            <img src="./assets/images/insta.png" alt="" class="footerconnect-img" />
+                        </a>
+                        <a href="#">
+                            <img src="./assets/images/gmail.png" alt="" class="footerconnect-img" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright">
+            <div class="container">
+                <p>&copy; COPYRIGHT2023. All right reserved.</p>
+            </div>
+        </div>
+    </footer>
+
+    <script src="./assets/js/index.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/
+  ionicons.js"></script>
+</body>
+
+</html>
---
+++ backend/moreproduct.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="./assets/css/moreproduct.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>More Product</title>
+</head>
+
+<body>
+    <header class="site-header">
+        <div class="container">
+            <div class="header-inner">
+                <img src="./assets/images/LOGO.png" class="sitebrand" alt="" />
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="#">Search</a></li>
+                        <li><a href="#">Cart</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="./loginin.php">LogIn</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <div class="moreproduct">
+        <div class="moreproduct-container">
+            <div class="more-product">
+                <h1 class="moreproduct-heading">Mobile Games</h1>
+            </div>
+
+            <div class="img-wrap">
+                <div class="moreproduct-content">
+                    <img src="./assets/images/img.jpg" alt="">
+                    <h5>More Product</h5>
+                </div>
+
+                <div class="moreproduct-content">
+                    <img src="./assets/images/img.jpg" alt="">
+                    <h5>More Product</h5>
+                </div>
+
+                <div class="moreproduct-content">
+                    <img src="./assets/images/img.jpg" alt="">
+                    <h5>More Product</h5>
+                </div>
+
+                <div class="moreproduct-content">
+                    <img src="./assets/images/img.jpg" alt="">
+                    <h5>More Product</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
