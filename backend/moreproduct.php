6e13730e33a8fba88e9f9d59b8cf5bb432dbab3a
GenjaLuffy <spawon551@gmail.com>
2023-08-18 15:08:14 +0545

    Complet

--- backend/moreproduct.php
+++ backend/moreproduct.php
@@ -1,4 +1,3 @@
-Shrestha Paw-won
 <?php include_once 'includes/header.php'; ?>
 <?php include 'connect.php'; ?>
 
@@ -17,16 +16,16 @@ Shrestha Paw-won
             // Display data in HTML
             if (mysqli_num_rows($result) > 0) {
                 while ($row = mysqli_fetch_assoc($result)) { ?>
-                    <div class="best-content">
-                        <figure>
-                            <a href="singleitem.php?id=<?php echo $row['id']; ?>">
-                                <img src="<?php echo $row['image_path']; ?>" alt="<?php echo $row['item_name']; ?>" />
-                            </a>
-                        </figure>
-                        <h5>
-                            <?php echo $row['item_name']; ?>
-                        </h5>
-                    </div>
+            <div class="best-content">
+                <figure>
+                    <a href="singleitem.php?id=<?php echo $row['id']; ?>">
+                        <img src="<?php echo $row['image_path']; ?>" alt="<?php echo $row['item_name']; ?>" />
+                    </a>
+                </figure>
+                <h5>
+                    <?php echo $row['item_name']; ?>
+                </h5>
+            </div>
             <?php
                 }
             } else {
@@ -58,16 +57,16 @@ Shrestha Paw-won
             if (mysqli_num_rows($result) > 0) {
                 while ($row = mysqli_fetch_assoc($result)) {
             ?>
-                    <div class="best-content">
-                        <figure>
-                            <a href="singleitem.php?media_id=<?php echo $row['id']; ?>">
-                                <img src="<?php echo $row['image_path']; ?>" alt="<?php echo $row['item_name']; ?>" />
-                            </a>
-                        </figure>
-                        <h5>
-                            <?php echo $row['item_name']; ?>
-                        </h5>
-                    </div>
+            <div class="best-content">
+                <figure>
+                    <a href="singleitem.php?media_id=<?php echo $row['id']; ?>">
+                        <img src="<?php echo $row['image_path']; ?>" alt="<?php echo $row['item_name']; ?>" />
+                    </a>
+                </figure>
+                <h5>
+                    <?php echo $row['item_name']; ?>
+                </h5>
+            </div>
             <?php
                 }
             } else {
@@ -121,12 +120,12 @@ Shrestha Paw-won
                 while ($row = mysqli_fetch_assoc($result)) {
                     echo '<div class="best-content">';
             ?>
-                    <figure>
+            <figure>
 
-                        <a href="singleitem.php?cards_id=<?php echo $row['id']; ?>">
-                            <img src="<?php echo $row['image_path']; ?>" alt="<?php echo $row['item_name']; ?>" />
-                        </a>
-                    </figure>
+                <a href="singleitem.php?cards_id=<?php echo $row['id']; ?>">
+                    <img src="<?php echo $row['image_path']; ?>" alt="<?php echo $row['item_name']; ?>" />
+                </a>
+            </figure>
             <?php
                     echo '<h5>' . $row['item_name'] . '</h5>';
                     echo '</div>';
@@ -155,4 +154,9 @@ Shrestha Paw-won
             <div class="mobilegame-content">
                 <img src="./assets/images/img.jpg" alt="" />
                 <h5>Playstation Gift Card</h5>
-            </div>
+            </div> -->
+        </div>
+    </div>
+</div>
+
+<?php include_once 'includes/footer.php'; ?>
